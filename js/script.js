a55067f78aacff5e8556bcfb52087ca80fd32b0f
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-07-15 21:13:05 +0300

    full task push

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+//1. Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку:
+const userNamesFIO = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
 
 let result = '';
 
-const strArr = userNames.join(",")
+const strArr = userNamesFIO.join(',')
 const upperStrArr = strArr.toUpperCase()
 for (let i = 0; i < strArr.length; i++){
     if (upperStrArr[i] === strArr[i]){
@@ -10,10 +11,46 @@ for (let i = 0; i < strArr.length; i++){
     }
 }
 
-let initials = result.split(",")
-initials.forEach((item, index, arr) => {
-    arr[index] += '.'
-} )
+const initials = result.split(",").sort((a,b)=>a.localeCompare(b)).map((item, index, arr) => arr[index] += '.')
 
-console.log(strArr)
 console.log(initials)
+
+//2. Задача на фільтрування масиву. Реалізуйте фільтрування імен які починаються з голосної двома способами:
+// 1) через умовну конструкцію
+// 2) через вбудований метод масивів
+
+let userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+
+// 1) через умовну конструкцію
+let filteredNamesNew = [];
+
+let literalStr = 'а о и е у ї є і у ю я';
+
+for (let i=0; i<userNames.length;i++){
+    if (literalStr.includes(userNames[i][0].toLowerCase())){
+        filteredNamesNew.push(userNames[i])
+    }
+}
+console.log(filteredNamesNew)
+// 2) через вбудований метод масивів
+let literal= ['а', 'о', 'и', 'е', 'ї', 'є', 'і', 'у', 'ю', 'я'];
+
+let filteredNames = userNames.filter(item => {return literal.includes(item[0].toLowerCase())})
+console.log(filteredNames);
+
+
+// 3. Задача на розворот числа:
+const currentMaxValue = 4589;
+
+let reverseMaxValue = Number(currentMaxValue.toString().split('').reverse().join(''))
+// reverseMaxValue
+console.log(reverseMaxValue)
+console.log(typeof reverseMaxValue);
+
+//Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:
+let sum = 1
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray = 1;
+resultsArray.flat(2).forEach(item => productOfArray *= item)
+
+console.log(productOfArray); // 24
